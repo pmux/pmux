10de060f7644971ef5f374daaeeb3ca1f52565d3
Travis Woodrow <traviswoodrow@email.arizona.edu>
2017-01-18 10:54:17 -0700

    adds email and updates version

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='pmux',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     author='Travis Woodrow <travis.woodrow@latitudeengineering.com>, Chasen Johnson <chase.johnson@latitudeengineering.com>',
     description='An effort toward making distributed, cross-language programs easier to write.',
+    url="https://github.com/pmux/pmux",
     install_requires=['nnpy', 'msgpack-python'],
     zip_safe=False,
 )
